9a617d33a51ca84046db920275c5d4a638ea6ed0
arthurmeirelessm <arthurmsesjdfdn@gmail.com>
2023-08-17 13:22:01 -0300

    ..

--- XUnit-proficiency/Repository/PayerRespository.cs
+++ XUnit-proficiency/Repository/PayerRespository.cs
@@ -1,6 +1,18 @@
-﻿namespace XUnit_proficiency.Repository
+﻿using XUnit_proficiency.Interfaces;
+using XUnit_proficiency.Models;
+
+namespace XUnit_proficiency.Repository
 {
-    public class PayerRespository
+    public class PayerRespository : IPayerRespository
     {
+        public void Add(Payer payer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Payer> Get()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
